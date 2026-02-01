232a74949f50c95f5f9be15644c1dd17e5524bda
Stanislav Rush <i@svrush.ru>
2026-02-02 00:58:10 +0700

    Попытка #7

--- tests/core/driver.py
+++ tests/core/driver.py
@@ -1,6 +1,12 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 def get_driver():
-    driver = webdriver.Remote(command_executor="http://localhost:4444/wd/hub")
+    options = Options()
+    options.add_argument("--no-sandbox")  # Обязательно для CI!
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+
+    driver = webdriver.Remote(command_executor="http://localhost:4444/wd/hub", options=options)
     return driver
