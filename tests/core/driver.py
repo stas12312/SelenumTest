3e7a46a7bed6ebc6c20b101cfa5ad787d810d186
Stanislav Rush <i@svrush.ru>
2026-02-02 01:15:23 +0700

    Попытка #9

--- tests/core/driver.py
+++ tests/core/driver.py
@@ -8,6 +8,8 @@ def get_driver():
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
 
-    # driver = webdriver.Remote(command_executor="http://localhost:4444/wd/hub", options=options)
-    driver = webdriver.Chrome(options=options)
+    try:
+        driver = webdriver.Chrome(options=options)
+    except:
+        driver = webdriver.Remote(command_executor="http://localhost:4444/wd/hub", options=options)
     return driver
